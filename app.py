63391b8646397a9b7a16652d0b6175343f54a71c
SaiKishore93654 <151427498+SaiKishore93654@users.noreply.github.com>
2025-06-04 18:13:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import bugsnag
+from bugsnag.flask import handle_exceptions
 
 app = Flask(__name__)
 
+bugsnag.configure(
+    api_key="YOUR_BUGSNAG_API_KEY",
+    project_root="/app",
+)
+
+handle_exceptions(app)
 @app.route("/")
 def home():
     return "Hello from Flask Docker app!"
